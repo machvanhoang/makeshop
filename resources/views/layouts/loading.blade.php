b691545fd5a4c2e378763291626abe5868053017
AGL_Mạch Văn Hoàng (ホアン) <81142674+machvanhoang@users.noreply.github.com>
2024-07-12 13:33:52 +0700

    Fix loading

--- resources/views/layouts/loading.blade.php
+++ resources/views/layouts/loading.blade.php
@@ -1,13 +1,8 @@
-{{--<div class="loading-fixed">--}}
-{{--    <div class="loading-main">--}}
-{{--        <div class="alert-text">--}}
-{{--            <p>同期プロセスには時間がかかります。<br> ブラウザを閉じずにお待ちください。</p>--}}
-{{--            <span class="loading"></span>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
 <div class="loading-fixed">
-    <div id="progress-bar" class="progress-bar">
-        <div class="progress-bar-fill"></div>
+    <div class="loading-main">
+        <div class="alert-text">
+            <p>同期プロセスには時間がかかります。<br> ブラウザを閉じずにお待ちください。</p>
+            <span class="loading"></span>
+        </div>
     </div>
 </div>
